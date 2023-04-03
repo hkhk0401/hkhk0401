daa12e01eb8cbdea3fe146b049921b6eacca0ae4
hkhk0401 <yhk4201@naver.com>
2023-04-03 12:11:37 +0900

    클래스와 마우스 인풋

---
+++ My project hk/Assets/scripts/0403/Test_007.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_007 : MonoBehaviour
+{
+
+
+    void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+    void CallName(string name)
+    {
+        Debug.Log("Hello" + name);
+    }
+
+    int Add(int a , int b)
+    {
+        int c = a + b;
+        return c;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SayHello();
+        SayHello();
+        CallName("Tom");
+        CallName("Sam");
+        int answer = Add(2, 5);
+        Debug.Log(answer);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
