dd25ae7f648d5e753124a9672b3f930e127416c0
hkhk0401 <@asd757412>
2023-05-08 13:09:00 +0900

    적생성

--- My project hk/Assets/scripts/Controller/ProjectileMove.cs
+++ My project hk/Assets/scripts/Controller/ProjectileMove.cs
@@ -9,22 +9,33 @@ public class ProjectileMove : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name== "Wall")
+        if (collision.gameObject.name == "Wall")
         {
             Destroy(this.gameObject);
         }
-        if(collision.gameObject.name == "Monster")
+        if (collision.gameObject.name == "Monster")
         {
             collision.gameObject.GetComponent<MonsterController>().Damanged(1);
             Destroy(this.gameObject);
         }
     }
-    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Monster")
+        {
+            other.gameObject.GetComponent<MonsterController>().Damanged(1);
+            Destroy(this.gameObject);
+        }
+    }
+
     private void FixedUpdate()
     {
-        float moveAmount = 3 * Time. fixedDeltaTime;
-
+        float moveAmount = 3 * Time.fixedDeltaTime;
         transform.Translate(launchDirection * moveAmount);
     }
 }
-
